574310da5301c695378e844fdc42642229b90d56
mattfan00 <kungfumo@gmail.com>
2020-05-08 16:41:50 -0400

    added in concept of separate rooms and login/register page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'),
       http = require('http'),
-      socketio = require('socket.io')
+      socketio = require('socket.io'),
+      bodyParser = require('body-parser')
 
 const app = express(),
       server = http.createServer(app),
@@ -9,24 +10,69 @@ const app = express(),
 
 app.use(express.static(__dirname + "/public"))
 
-app.get('/', (req, res) => {
-  res.render('index.html')
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+
+const chats = [
+  {
+    id: 1,
+    name: 'Pig'
+  },
+  {
+    id: 2,
+    name: 'Dog'
+  }
+]
+
+app.get('/login', (req, res) => {
+  res.render('login')
 })
 
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.redirect('/chat')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  res.redirect('/chat')
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.redirect('/chat')
+})
+
+app.get('/chat', (req, res) => {
+  res.redirect('/chat/' + chats[0].id)
+})
+
+app.get('/chat/:id', (req, res) => {
+  res.render('index', {chats})
+})
+
+
 io.on('connection', (socket) => {
-  // Send message to the user that just joined the chat 
-  socket.emit('message', 'Welcome to my chat application')
+  socket.on('joinChat', (chatId) => {
+    socket.join(chatId)
+    // Send message to the user that just joined the chat 
+    socket.emit('message', 'Welcome to chat room ' + chatId)
 
-  // Send message to the whole lobby except the user that joined
-  socket.broadcast.emit('chat message', 'A new user has joined the chat')
-
-  // Send message to everyone that a user has left the chat 
-  socket.on('disconnect', () => {
-    io.emit('chat message', 'A user has left the chat')
+    // Send message to the whole lobby except the user that joined
+    socket.to(chatId).broadcast.emit('message', 'A new user has joined the chat')
   })
 
+  // Send message to everyone that a user has left the chat 
+  // socket.on('disconnect', () => {
+  //   io.emit('message', 'A user has left the chat')
+  // })
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+    io.to(msg.chatId).emit('message', msg.value)
   });
 });
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,23 +3,28 @@ const socket = io()
 const chatForm = document.getElementById('chat-form')
 const messages = document.getElementById('messages')
 
+const chatId = window.location.pathname.split('/')[2]
+
+socket.emit('joinChat', chatId)
+
 socket.on('message', (msg) => {
-  console.log(msg)
+  outputMessage(msg)
 })
 
-socket.on('chat message', (msg) => {
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  msg = e.target.elements.msg
+  socket.emit('chat message', {chatId, value: msg.value})
+  msg.value = ''
+})
+
+function outputMessage(msg) {
   var div = document.createElement('div')
   div.innerHTML = `
     <div class='message'>
       ${msg}
     </div>`
   messages.appendChild(div)
-})
-
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  msg = e.target.elements.msg
-  socket.emit('chat message', msg.value)
-  msg.value = ''
-})
+}
